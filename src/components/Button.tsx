53f72780036d462b5ab977fec33b60bf441c7991
fofx <identityofsine@gmail.com>
2024-04-04 17:40:47 -0400

    footer

---
+++ src/components/Button.tsx
@@ -0,0 +1,19 @@
+type ButtonProps = {
+	text: string;
+	icon_type?: 'phone' | 'default' | 'email';
+	primary?: boolean;
+	onClick?: () => void; //?
+}
+
+function Button({ text, primary = true, icon_type = "default", onClick = () => { } }: ButtonProps) {
+
+	return (
+		<button className={`button ${primary ? 'primary' : ''}`} >
+			<img src={`/icons/${icon_type}.svg`} alt={icon_type} />
+			{text}
+		</button>
+	)
+
+}
+
+export default Button;
