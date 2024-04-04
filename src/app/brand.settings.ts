5478828f7141f5d21f99df3a15ca107cebedbc43
fofx <identityofsine@gmail.com>
2024-04-04 17:32:20 -0400

    added brand settings js

---
+++ src/app/brand.settings.ts
@@ -0,0 +1,6 @@
+const brandSettings = {
+	name: 'Exquisite Gallery Photography',
+	theme: 'light',
+}
+
+export default brandSettings;	
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,19 @@
+import brandSettings from '@/app/brand.settings'
 import '../styles/navbar.scss'
 
 export function NavBar() {
 	return (
-		<nav>
-
+		<nav className="relative">
+			<div className="content flex align-center justify-between margin-width center-margin">
+				<div className="flex align-center logo-gap">
+					<p>{brandSettings.name}</p>
+				</div>
+				<div className="flex align-center nav-gap">
+					<a href="/#">Services</a>
+					<a href="/#">About</a>
+					<a href="/#">Contact</a>
+				</div>
+			</div>
 		</nav>
 	)
 }
