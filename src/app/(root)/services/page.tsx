af031083f75137607fb0be27e00b011342ea0050
fofx <identityofsine@gmail.com>
2024-04-06 20:33:39 -0400

    services page done

--- src/app/(root)/services/page.tsx
+++ src/app/(root)/services/page.tsx
@@ -7,14 +7,14 @@ export default function ServicesPage() {
 		<main className="services margin-width center-margin">
 			<h2>Our Services</h2>
 			<section className="grid-container ">
-				<a href={brandSettings.path + 'services/photography#wedding'} className="grid-item-1">
-					<GradientCard className="fill-width fill-height" imgSrc="/images/wwd-1.png" title="Weddings" />
+				<a href={brandSettings.path + 'services/photography'} className="grid-item-1">
+					<GradientCard className="fill-width fill-height" imgSrc="/images/services-1.png" title="Weddings" />
 				</a>
-				<a href={brandSettings.path + 'services/photography#pep'}>
-					<GradientCard className="fill-width fill-height" imgSrc="/images/wwd-2.png" title="Parties" />
+				<a href={brandSettings.path + 'services/videography'}>
+					<GradientCard className="fill-width fill-height" imgSrc="/images/services-2.png" title="Videography" />
 				</a>
-				<a href={brandSettings.path + 'services/photography#pep'}>
-					<GradientCard className="fill-width fill-height" imgSrc="/images/wwd-2.png" title="Parties" />
+				<a href={brandSettings.path + 'about#dj'}>
+					<GradientCard className="fill-width fill-height" imgSrc="/images/services-3.png" title="DJ" />
 				</a>
 			</section>
 		</main>
