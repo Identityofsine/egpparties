9f26339d6bdcc7eeba73bbf157b282b9da324bbf
fofx <identityofsine@gmail.com>
2024-04-07 21:58:14 -0400

    about page tackled

--- src/app/(root)/about/page.tsx
+++ src/app/(root)/about/page.tsx
@@ -11,29 +11,29 @@ export default function AboutPage() {
 					<h3>Or EGP</h3>
 				</div>
 				<p className="margin-top-04">Key moments in our lives act as milestones, encapsulating cherished memories that define us.</p>
-				<div className="flex justify-between fill-width image-row margin-top-06">
-					<img src="/images/about-1.png" alt="About 1" />
-					<img src="/images/about-2.png" alt="About 2" />
-					<img src="/images/about-3.png" alt="About 3" />
+				<div className="flex justify-between fill-width image-row margin-top-06 m-flex-column m-fill-width m-gap-10">
+					<img className="m-width-fill m-max-width" src="/images/about-1.png" alt="About 1" />
+					<img className="m-width-fill m-max-width" src="/images/about-2.png" alt="About 2" />
+					<img className="m-width-fill m-max-width" src="/images/about-3.png" alt="About 3" />
 				</div>
 			</section>
 			<section className="based-in">
 				<h2>Based In Long Island, New York</h2>
-				<p className="margin-top-01"><b>Exquisite Gallery Photography</b> extends its artistry and dedication to clients throughout the entirety of the Empire State. From the vibrant streets of New York City to the serene shores of Long Island, our passion for capturing the beauty of life knows no bounds.</p>
-				<img className="margin-top-04" src="/images/about-4.png" alt="About 4" />
+				<p className="margin-top-01 m-margin-top-03"><b>Exquisite Gallery Photography</b> extends its artistry and dedication to clients throughout the entirety of the Empire State. From the vibrant streets of New York City to the serene shores of Long Island, our passion for capturing the beauty of life knows no bounds.</p>
+				<img className="margin-top-04 m-margin-top-07" src="/images/about-4.png" alt="About 4" />
 			</section>
 			<section className="photography-to-us">
 				<h4>
 					What Photography Is To Us
 				</h4>
-				<div className="flex container">
+				<div className="flex container m-flex-column">
 					<div className="flex column left">
-						<p>
+						<p className="m-margin-top-02">
 							Embarking on a legacy: continuing the proud family tradition of over 25 years, we pledge to uphold the sacred honor of photography, one image at a time.
 						</p>
 						<div className="sub-block">
 							<h2>We Never Want To Miss "It"</h2>
-							<p>
+							<p className="m-margin-top-02">
 								Whether “it” is the enchantment of a wedding celebration, the exuberance of a sweet 16 bash, the warmth of a birthday gathering, or any other special event that fills hearts with joy. At <b>Exquisite Gallery Photography</b>, we are committed to capturing every “it” moment with precision, passion and artistry, ensuring that your cherished memories last a lifetime.
 							</p>
 							<a href={brandSettings.path + "services/photography"}>
@@ -50,9 +50,9 @@ export default function AboutPage() {
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel urna porta nisi rutrum vulputate a id ex. Sed consequat gravida mi. Mauris ut augue auctor augue tempor condimentum. Duis porttitor lacinia massa vel tempus.
 				</p>
 				<div className="flex column button-gap margin-top-06">
-					<Button primary={true} text="Get A Free Consultation" />
-					<Button primary={false} text="Contact Us Directly" />
-					<Button primary={false} text="See Our Services" />
+					<Button className="m-width-fill" primary={true} text="Get A Free Consultation" />
+					<Button className="m-width-fill" primary={false} text="Contact Us Directly" />
+					<Button className="m-width-fill" primary={false} text="See Our Services" />
 				</div>
 			</section>
 		</main>
