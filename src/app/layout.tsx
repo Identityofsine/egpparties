d1e6cbf83e737c0e06f70a2a352ee61280b21bcc
Kevin <identityofsine@gmail.com>
2024-04-12 22:07:06 -0400

    fixed comment bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,7 +48,7 @@ export default function RootLayout({
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', G-LYZ0Q6LZJN);
+              gtag('config', 'G-LYZ0Q6LZJN');
           `}
 			</Script>
 		</html>
